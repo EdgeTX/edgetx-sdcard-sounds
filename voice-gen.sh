383345e8154825894cb3843b30780c4798e336ee
Eder Nucci <eder@greenpass.com.br>
2021-06-21 09:00:42 -0300

    https://github.com/EdgeTX/edgetx-sdcard-sounds/issues/1

--- voice-gen.sh
+++ voice-gen.sh
@@ -3,13 +3,14 @@
 generate_lang () {
   while read line
   do
+      destination=`echo -n $line | awk -F ';' '{print $1}'`
       filename=`echo -n $line | awk -F ';' '{print $2}'`
       text=`echo -n $line | awk -F ';' '{print $3}'`
-      if test -f SOUNDS/$3/SYSTEM/$filename; then
+      if test -f $destination/$filename; then
           echo "File $filename already exists. Skipping."
       else
           echo "File $filename does not exists. Creating."
-          spx synthesize --text \""$text"\" --voice $2 --audio output SOUNDS/$3/SYSTEM/$filename
+          spx synthesize --text \""$text"\" --voice $2 --audio output $destination/$filename
       fi
   done < $1
 }
