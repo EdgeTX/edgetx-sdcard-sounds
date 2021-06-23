136a2db0fb4cad61ad61856561bbf5e2363d5e60
Eder Nucci <eder@greenpass.com.br>
2021-06-23 13:26:10 -0300

    Removing the word taranis from files

--- generate.sh
+++ generate.sh
@@ -3,11 +3,11 @@
 # include voice generator script
 . $(dirname "$0")/voice-gen.sh
 
-generate_lang voices/en-US-taranis.csv en-IE-EmilyNeural en
-generate_lang voices/pt-PT-taranis.csv pt-BR-FranciscaNeural pt
-generate_lang voices/es-ES-taranis.csv es-ES-ElviraNeural es
-generate_lang voices/it-IT-taranis.csv it-IT-ElsaNeural it
-generate_lang voices/de-DE-taranis.csv de-DE-KatjaNeural de
-generate_lang voices/fr-FR-taranis.csv fr-FR-DeniseNeural fr
-generate_lang voices/ru-RU-taranis.csv ru-RU-SvetlanaNeural ru
-generate_lang voices/cs-CZ-taranis.csv cs-CZ-VlastaNeural cz
+generate_lang voices/en-US.csv en-IE-EmilyNeural en
+generate_lang voices/pt-PT.csv pt-BR-FranciscaNeural pt
+generate_lang voices/es-ES.csv es-ES-ElviraNeural es
+generate_lang voices/it-IT.csv it-IT-ElsaNeural it
+generate_lang voices/de-DE.csv de-DE-KatjaNeural de
+generate_lang voices/fr-FR.csv fr-FR-DeniseNeural fr
+generate_lang voices/ru-RU.csv ru-RU-SvetlanaNeural ru
+generate_lang voices/cs-CZ.csv cs-CZ-VlastaNeural cz
