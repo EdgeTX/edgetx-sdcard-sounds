43fa46031fbc430a4aa21bc08a404748c2eed9ce
Peter Feerick <peter.feerick@gmail.com>
2022-04-14 18:54:32 +1000

    Shellcheck, normalise to release folder

--- release.sh
+++ release.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-version=2.5.4
+: "${VERSION:=2.54}"
+FFMPEG_FLAGS=(-hide_banner -loglevel error)
+: "${FFMPEG_AF_FLAGS:=volume=6.0dB,silenceremove=start_periods=1:start_silence=0.1:start_threshold=-50dB,areverse,silenceremove=start_periods=1:start_silence=0.1:start_threshold=-50dB,areverse}"
 
-# make normalized folders
-find SOUNDS -type d | xargs -I % mkdir -p normalized/%
+# delete release folder if already exists
+[[ -e release ]] && rm -rf release
 
-# normalize each file and replace source
-for file in $(find SOUNDS -type f)
-do
-    ffmpeg -i $file -af "volume=6.0dB,silenceremove=start_periods=1:start_silence=0.1:start_threshold=-50dB,areverse,silenceremove=start_periods=1:start_silence=0.1:start_threshold=-50dB,areverse" normalized/$file && \
-    mv -f normalized/$file $file
+echo "VERSION         : ${VERSION}"
+echo "FFMPEG_FLAGS    :" "${FFMPEG_FLAGS[@]}"
+echo "FFMPEG_AF_FLAGS : ${FFMPEG_AF_FLAGS}"
+
+echo "Creating release folders..."
+find SOUNDS -type d -exec mkdir -p "release/{}" \;
+
+NUM_OF_FILES=$(find SOUNDS -type f | wc -l)
+echo "Normalizing $NUM_OF_FILES files..."
+shopt -s globstar nullglob
+for file in SOUNDS/**/*.wav; do
+    [[ -e "$file" ]] || break  # handle the case of no *.wav files
+    echo "$file"
+    ffmpeg "${FFMPEG_FLAGS[@]}" -i "$file" -af "$FFMPEG_AF_FLAGS" "release/$file"
 done
 
-# compress the release files
-for lang in $(ls SOUNDS)
-do
-    find SOUNDS/$lang -type f | zip -@ edgetx-sdcard-sounds-$lang-$version.zip
+echo "Preparing release zip files..."
+cd release || exit
+for lang in SOUNDS/*/; do
+    ZIPLANG=$(basename "$lang")
+    [[ -e "$lang" ]] || break  # handle the case of no *.wav files
+    echo Creating "edgetx-sdcard-sounds-$ZIPLANG-$VERSION.zip"
+    find "$lang" -type f | zip -q -@ "edgetx-sdcard-sounds-$ZIPLANG-$VERSION.zip"
 done
