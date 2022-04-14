3462de719c6ab3e76dc969c639598624f716c4f9
Peter Feerick <peter.feerick@gmail.com>
2022-04-14 19:31:12 +1000

    Use `ffmpeg-normalize` rather than fixed db increase

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 
 : "${VERSION:=2.54}"
 FFMPEG_FLAGS=(-hide_banner -loglevel error)
-: "${FFMPEG_AF_FLAGS:=volume=6.0dB,silenceremove=start_periods=1:start_silence=0.1:start_threshold=-50dB,areverse,silenceremove=start_periods=1:start_silence=0.1:start_threshold=-50dB,areverse}"
+: "${FFMPEG_AF_FLAGS:=silenceremove=start_periods=1:start_silence=0.1:start_threshold=-50dB,areverse,silenceremove=start_periods=1:start_silence=0.1:start_threshold=-50dB,areverse}"
 
 # delete release folder if already exists
 [[ -e release ]] && rm -rf release
@@ -15,12 +15,13 @@ echo "Creating release folders..."
 find SOUNDS -type d -exec mkdir -p "release/{}" \;
 
 NUM_OF_FILES=$(find SOUNDS -type f | wc -l)
-echo "Normalizing $NUM_OF_FILES files..."
+echo "Trim and normalize $NUM_OF_FILES files..."
 shopt -s globstar nullglob
 for file in SOUNDS/**/*.wav; do
     [[ -e "$file" ]] || break  # handle the case of no *.wav files
     echo "$file"
     ffmpeg "${FFMPEG_FLAGS[@]}" -i "$file" -af "$FFMPEG_AF_FLAGS" "release/$file"
+    ffmpeg-normalize "release/$file" -o "release/$file" -f -nt peak -t 0
 done
 
 echo "Preparing release zip files..."
