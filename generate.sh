98bf58f3c409635bf3da533f089433834a342413
Peter Feerick <peter.feerick@gmail.com>
2022-04-15 21:32:24 +1000

    Add more EN variants

--- generate.sh
+++ generate.sh
@@ -3,7 +3,11 @@
 # include voice generator script
 . "$(dirname "$0")/voice-gen.sh"
 
-generate_lang voices/en-US.csv en-IE-EmilyNeural en
+generate_lang voices/en-IE-Emily.csv en-IE-EmilyNeural en
+generate_lang voices/en-US-Sara.csv en-US-SaraNeural en_US-sara
+generate_lang voices/en-US-Guy.csv en-US-GuyNeural en_US-guy
+generate_lang voices/en-GB-Libby.csv en-GB-LibbyNeural en_gb-libby
+generate_lang voices/en-GB-Ryan.csv en-GB-RyanNeural en_gb-ryan
 generate_lang voices/pt-PT.csv pt-BR-FranciscaNeural pt
 generate_lang voices/es-ES.csv es-ES-ElviraNeural es
 generate_lang voices/it-IT.csv it-IT-ElsaNeural it
@@ -11,5 +15,3 @@ generate_lang voices/de-DE.csv de-DE-KatjaNeural de
 generate_lang voices/fr-FR.csv fr-FR-DeniseNeural fr
 generate_lang voices/ru-RU.csv ru-RU-SvetlanaNeural ru
 generate_lang voices/cs-CZ.csv cs-CZ-VlastaNeural cz
-generate_lang voices/en-GB-Libby.csv en-GB-LibbyNeural en_gb-libby
-generate_lang voices/en-GB-Ryan.csv en-GB-RyanNeural en_gb-ryan
