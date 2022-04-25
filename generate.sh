3c06ead7c3dd49d473239aa290275333dd8d935a
Jorge Castillo <jorge.castillo.d@gmail.com>
2022-04-25 06:03:17 -0400

    feat: Chilean Spanish pack added (#22)
    * Create es-CL-Catalina.csv
    
    chilean spanish, made translating the us sara file
    
    * fix the generation scripts
    
    Co-authored-by: Jorge Castillo <jcastillo@cognitiva.la>

--- generate.sh
+++ generate.sh
@@ -10,6 +10,7 @@ generate_lang voices/en-GB-Libby.csv en-GB-LibbyNeural en_gb-libby
 generate_lang voices/en-GB-Ryan.csv en-GB-RyanNeural en_gb-ryan
 generate_lang voices/pt-PT.csv pt-BR-FranciscaNeural pt
 generate_lang voices/es-ES.csv es-ES-ElviraNeural es
+generate_lang voices/es-CL-Catalina.csv es-CL-CatalinaNeural es_cl-catalina
 generate_lang voices/it-IT.csv it-IT-ElsaNeural it
 generate_lang voices/de-DE.csv de-DE-KatjaNeural de
 generate_lang voices/fr-FR.csv fr-FR-DeniseNeural fr
