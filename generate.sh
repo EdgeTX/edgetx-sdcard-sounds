e237a660d54d5b5b177800189c8b20470c6ef3fe
Peter Feerick <peter.feerick@gmail.com>
2022-05-02 15:48:22 +1000

    Update generator script

--- generate.sh
+++ generate.sh
@@ -10,9 +10,10 @@ find "$(dirname "$0")" -name "*.log" -delete
 ./voice-gen.py voices/en-US.csv en-US-GuyNeural en_us-guy
 ./voice-gen.py voices/pt-PT.csv pt-BR-FranciscaNeural pt
 ./voice-gen.py voices/es-ES.csv es-ES-ElviraNeural es
-./voice-gen.py voices/es-CL-Catalina.csv es-CL-CatalinaNeural es_cl-catalina
+./voice-gen.py voices/es-CL.csv es-CL-CatalinaNeural es_cl-catalina
 ./voice-gen.py voices/it-IT.csv it-IT-ElsaNeural it
 ./voice-gen.py voices/de-DE.csv de-DE-KatjaNeural de
 ./voice-gen.py voices/fr-FR.csv fr-FR-DeniseNeural fr
 ./voice-gen.py voices/ru-RU.csv ru-RU-SvetlanaNeural ru
 ./voice-gen.py voices/cs-CZ.csv cs-CZ-VlastaNeural cz
+# ./voice-gen.py voices/zh-CN.csv
