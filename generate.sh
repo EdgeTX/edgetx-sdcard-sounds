51bce5185433d42e6d9150ebef597c12e498a07a
Peter Feerick <peter.feerick@gmail.com>
2022-05-02 15:40:43 +1000

    Change to python based generator * Allows for more robust CSV file handling * Can use azure speechsdk for audio generation

--- generate.sh
+++ generate.sh
@@ -3,19 +3,16 @@
 # delete logs from previous pass
 find "$(dirname "$0")" -name "*.log" -delete
 
-# include voice generator script
-. "$(dirname "$0")/voice-gen.sh"
-
-generate_lang voices/en-GB.csv en-IE-EmilyNeural en
-generate_lang voices/en-GB.csv en-GB-LibbyNeural en_gb-libby
-generate_lang voices/en-GB.csv en-GB-RyanNeural en_gb-ryan
-generate_lang voices/en-US.csv en-US-SaraNeural en_us-sara
-generate_lang voices/en-US.csv en-US-GuyNeural en_us-guy
-generate_lang voices/pt-PT.csv pt-BR-FranciscaNeural pt
-generate_lang voices/es-ES.csv es-ES-ElviraNeural es
-generate_lang voices/es-CL-Catalina.csv es-CL-CatalinaNeural es_cl-catalina
-generate_lang voices/it-IT.csv it-IT-ElsaNeural it
-generate_lang voices/de-DE.csv de-DE-KatjaNeural de
-generate_lang voices/fr-FR.csv fr-FR-DeniseNeural fr
-generate_lang voices/ru-RU.csv ru-RU-SvetlanaNeural ru
-generate_lang voices/cs-CZ.csv cs-CZ-VlastaNeural cz
+./voice-gen.py voices/en-GB.csv en-IE-EmilyNeural en
+./voice-gen.py voices/en-GB.csv en-GB-LibbyNeural en_gb-libby
+./voice-gen.py voices/en-GB.csv en-GB-RyanNeural en_gb-ryan
+./voice-gen.py voices/en-US.csv en-US-SaraNeural en_us-sara
+./voice-gen.py voices/en-US.csv en-US-GuyNeural en_us-guy
+./voice-gen.py voices/pt-PT.csv pt-BR-FranciscaNeural pt
+./voice-gen.py voices/es-ES.csv es-ES-ElviraNeural es
+./voice-gen.py voices/es-CL-Catalina.csv es-CL-CatalinaNeural es_cl-catalina
+./voice-gen.py voices/it-IT.csv it-IT-ElsaNeural it
+./voice-gen.py voices/de-DE.csv de-DE-KatjaNeural de
+./voice-gen.py voices/fr-FR.csv fr-FR-DeniseNeural fr
+./voice-gen.py voices/ru-RU.csv ru-RU-SvetlanaNeural ru
+./voice-gen.py voices/cs-CZ.csv cs-CZ-VlastaNeural cz
---
+++ voice-gen.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+import argparse
+import csv
+import os
+import sys
+import time
+import subprocess
+from pathlib import Path
+
+import azure.cognitiveservices.speech as speechsdk
+
+
+def init_argparse() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        usage="%(prog)s [FILE] [VOICE] [LANGDIR]",
+        description="Generate voice packs from CSV list."
+    )
+
+    parser.add_argument(
+        "-v", "--version", action="version",
+        version=f"{parser.prog} version 1.0.0"
+    )
+
+    parser.add_argument('file',
+                        type=str,
+                        help="CSV Translation file"
+                        )
+
+    parser.add_argument('voice',
+                        type=str,
+                        help="Voice to use"
+                        )
+
+    parser.add_argument('langdir',
+                        type=str,
+                        help="Language subfolder"
+                        )
+
+    parser.add_argument('-s',
+                        '--delay',
+                        type=int,
+                        help="Sleep time processing each translation",
+                        required=False,
+                        default='3'
+                        )
+
+    return parser
+
+
+def main() -> None:
+    parser = init_argparse()
+    args = parser.parse_args()
+
+    csv_file = args.file
+    csv_rows = 0
+    voice = args.voice
+    langdir = args.langdir
+    basedir = os.path.dirname(os.path.abspath(__file__))
+    outdir = ""
+    delay_time = args.delay
+
+    try:
+        speech_key = os.environ['COGNITIVE_SERVICE_API_KEY']
+        service_region = os.environ['SERVICE_REGION']
+    except KeyError:
+        print("ERROR: Please set the environment variables for Speech and Service Region")
+        sys.exit(1)
+
+    speech_config = speechsdk.SpeechConfig(
+        subscription=speech_key, region=service_region)
+
+    if not(os.path.isfile(csv_file)):
+        print("Error: voice file not found")
+        sys.exit(1)
+
+    # Get number of rows in CSV file
+    with open(csv_file) as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        csv_rows = sum(1 for row in reader)
+
+    # Process CSV file
+    with open(csv_file, 'rt') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        line_count = 0
+        for row in reader:
+            if line_count == 0:
+                # print(f'Column names are {", ".join(row)}')
+                line_count += 1
+                csv_rows -= 1
+            else:
+                if row[4] is None or row[4] == "":
+                    outdir = os.path.join(basedir, "SOUNDS", langdir)
+                else:
+                    outdir = os.path.join(basedir, "SOUNDS", langdir, row[4])
+                en_text = row[1]
+                text = row[2]
+                filename = row[5]
+                outfile = os.path.join(outdir, filename)
+
+                if not os.path.exists(outdir):
+                    os.makedirs(outdir)
+
+                if not os.path.isfile(outfile):
+                    print(
+                        f'[{line_count}/{csv_rows}] Translate "{en_text}" to "{text}", save as "{outdir}/{filename}".')
+                    speech_config.speech_synthesis_voice_name = voice
+                    audio_config = speechsdk.audio.AudioOutputConfig(
+                        filename=outfile)
+                    synthesizer = speechsdk.SpeechSynthesizer(
+                        speech_config=speech_config, audio_config=audio_config)
+                    synthesizer.speak_text_async(text)
+                    time.sleep(delay_time)
+                else:
+                    print(
+                        f'[{line_count}/{csv_rows}] Skipping "{filename}" as already exists.')
+
+                line_count += 1
+
+        print(f'Finished processing {csv_rows} entries from "{csv_file}".')
+
+
+if __name__ == "__main__":
+    main()
