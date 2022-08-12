3e272b5ba0f5d6ee4a05e937b3df9692fcbd961e
Peter Feerick <peter.feerick@gmail.com>
2022-08-12 13:16:53 +1000

    feat: First basic build check - filename length

---
+++ build-checks.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+shopt -s globstar nullglob
+
+echo "File name length checks..."
+
+for file in SOUNDS/**/*.wav
+do 
+    filename=$(basename "$file")
+    filename="${filename%.*}"
+    # extension="${filename##*.}"
+    dir=$(dirname "$file")
+    dir=$(basename "$dir")
+
+    if [ "$dir" == "YAAPU" ] || [ "$dir" == "INAV" ] || [ "$dir" == "BETAFLIGHT" ]; then
+        continue
+    fi
+
+    if [ "$dir" != "SYSTEM" ]; then
+        if (( ${#filename} > 6 )); then
+            echo "Filename is too long: $file"
+            continue
+        fi
+    else
+        if (( ${#filename} > 8 )); then
+            echo "Filename is too long: $file"
+            continue
+        fi
+
+    fi
+done
