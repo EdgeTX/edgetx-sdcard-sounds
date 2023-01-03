5a40d2bc9ded837b8219d50519ae7c566775cbc0
Peter Feerick <peter.feerick@gmail.com>
2023-01-03 10:13:53 +1000

    fix: Move Swedish files from SV to SE

--- generate.sh
+++ generate.sh
@@ -37,8 +37,8 @@ find "$(dirname "$0")" -name "*.log" -delete
 ./voice-gen.py voices/ja-JP.csv ja-JP-NanamiNeural jp || exit 1
 ./voice-gen.py voices/pt-PT.csv pt-BR-FranciscaNeural pt || exit 1
 ./voice-gen.py voices/ru-RU.csv ru-RU-SvetlanaNeural ru || exit 1
-./voice-gen.py voices/sv-SE.csv sv-SE-SofieNeural sv --pitch "dn10%" --rate "dn0.9" || exit 1
-./voice-gen.py voices/sv-SE_scripts.csv sv-SE-SofieNeural sv/SCRIPTS --pitch "dn10%" --rate "dn0.9" || exit 1
+./voice-gen.py voices/sv-SE.csv sv-SE-SofieNeural se --pitch "dn10%" --rate "dn0.9" || exit 1
+./voice-gen.py voices/sv-SE_scripts.csv sv-SE-SofieNeural se/SCRIPTS --pitch "dn10%" --rate "dn0.9" || exit 1
 ./voice-gen.py voices/zh-CN.csv zh-CN-XiaoxiaoNeural cn || exit 1
 ./voice-gen.py voices/zh-TW.csv zh-TW-HsiaoChenNeural tw || exit 1
 ./voice-gen.py voices/zh-HK.csv zh-HK-HiuGaaiNeural hk --rate "dn1.2" || exit 1
