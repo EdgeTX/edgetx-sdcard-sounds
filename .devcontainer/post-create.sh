fc52e48d03287c22899a7b5f61f825c2fdd8102b
Peter Feerick <peter.feerick@gmail.com>
2024-02-14 02:32:56 +0000

    chore: .NET, SPX and OpenSSL v1 needed

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# Build and install OpenSSL 1.1.1u
+wget -O - https://www.openssl.org/source/openssl-1.1.1u.tar.gz | tar zxf -
+cd openssl-1.1.1u
+./config --prefix=/usr/local
+make -j $(nproc)
+sudo make install_sw install_ssldirs
+sudo ldconfig
+cd .. && sudo rm -R openssl-1.1.1u
+
 # Install .NET 6.0 SDK and the Speech CLI tool
-# sudo apt-get update && sudo apt-get install -y dotnet-sdk-6.0
-# dotnet tool install --global Microsoft.CognitiveServices.Speech.CLI
+sudo apt-get update && sudo apt-get install -y dotnet-sdk-6.0
+dotnet tool install --global Microsoft.CognitiveServices.Speech.CLI
 
 # Install the Azure Speech SDK for Python
 python -m pip install azure-cognitiveservices-speech
