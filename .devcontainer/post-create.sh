d624a57c6e15eec4b53b12267bc0679a481f965b
Peter Feerick <peter.feerick@gmail.com>
2024-02-14 01:29:46 +0000

    feat: Add VSCode devcontainer config

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install .NET 6.0 SDK and the Speech CLI tool
+# sudo apt-get update && sudo apt-get install -y dotnet-sdk-6.0
+# dotnet tool install --global Microsoft.CognitiveServices.Speech.CLI
+
+# Install the Azure Speech SDK for Python
+python -m pip install azure-cognitiveservices-speech
