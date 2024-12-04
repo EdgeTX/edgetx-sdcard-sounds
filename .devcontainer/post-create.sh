6a03c4d9f9a964bfaefffe2137a9f90e4fadf6dd
Peter Feerick <peter.feerick@gmail.com>
2024-12-04 03:33:39 +0000

    chore: change packages and dependencies

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# Build and install OpenSSL 1.1.1u
-wget -O - https://www.openssl.org/source/openssl-1.1.1u.tar.gz | tar zxf -
-cd openssl-1.1.1u
-./config --prefix=/usr/local
-make -j $(nproc)
-sudo make install_sw install_ssldirs
-sudo ldconfig
-cd .. && sudo rm -R openssl-1.1.1u
+# Install required packages
+sudo apt-get update &> /dev/null && sudo apt-get install -y build-essential ca-certificates libasound2-dev libssl-dev python3 python3-pip python3-venv python-is-python3 dotnet-sdk-8.0 ffmpeg
 
-# Install .NET 6.0 SDK, Speech CLI tool & ffmpeg for GLaDOS 
-sudo apt-get update &> /dev/null && sudo apt-get install -y dotnet-sdk-6.0 ffmpeg
-dotnet tool install --global Microsoft.CognitiveServices.Speech.CLI --version 1.42.0
+# Install Azure Speech CLI tool
+dotnet tool install --global Microsoft.CognitiveServices.Speech.CLI --version 1.44.0
+
+# Activate virtual environment
+python3 -m venv .venv
 
 # Install the Azure Speech SDK for Python
-python -m pip install azure-cognitiveservices-speech==1.42.0 ffmpeg-normalize
+.venv/bin/pip install azure-cognitiveservices-speech==1.44.0 ffmpeg-normalize
+
+# Activate the virtual environment
+source .venv/bin/activate
