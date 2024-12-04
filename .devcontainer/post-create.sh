0508bd93dd534b265a69eacc98fcb71725f8aeba
Peter Feerick <peter.feerick@gmail.com>
2024-12-04 03:33:39 +0000

    chore: change packages and dependencies

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Build and install OpenSSL 1.1.1u
-wget -O - https://www.openssl.org/source/openssl-1.1.1u.tar.gz | tar zxf -
-cd openssl-1.1.1u
-./config --prefix=/usr/local
-make -j $(nproc)
-sudo make install_sw install_ssldirs
-sudo ldconfig
-cd .. && sudo rm -R openssl-1.1.1u
+# Install required packages
+sudo apt-get update
+sudo apt-get install -y build-essential ca-certificates libasound2-dev libssl-dev python3 python3-pip python3-venv python-is-python3 dotnet-sdk-8.0
 
-# Install .NET 6.0 SDK and the Speech CLI tool
-sudo apt-get update && sudo apt-get install -y dotnet-sdk-6.0
+# Install Azure Speech CLI tool
 dotnet tool install --global Microsoft.CognitiveServices.Speech.CLI
 
+# Activate virtual environment
+python3 -m venv .venv
+
 # Install the Azure Speech SDK for Python
-python -m pip install azure-cognitiveservices-speech
+.venv/bin/pip install azure-cognitiveservices-speech
+
+# Activate the virtual environment
+source .venv/bin/activate
