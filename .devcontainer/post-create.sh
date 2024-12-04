c3e10e96b5588a4fd463ac6129604b5697570b74
Peter Feerick <peter.feerick@gmail.com>
2024-12-04 05:03:50 +0000

    chore: ffmpeg needed for glados voice processing

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,7 +2,7 @@
 
 # Install required packages
 sudo apt-get update
-sudo apt-get install -y build-essential ca-certificates libasound2-dev libssl-dev python3 python3-pip python3-venv python-is-python3 dotnet-sdk-8.0
+sudo apt-get install -y build-essential ca-certificates libasound2-dev libssl-dev python3 python3-pip python3-venv python-is-python3 dotnet-sdk-8.0 ffmpeg
 
 # Install Azure Speech CLI tool
 dotnet tool install --global Microsoft.CognitiveServices.Speech.CLI
