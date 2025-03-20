5f62bdbd221fc3b8b80f726ab8cf73fe67f6ac30
Peter Feerick <peter.feerick@gmail.com>
2025-03-20 01:17:49 +0000

    chore: make `generate.sh` more DRY

--- generate.sh
+++ generate.sh
@@ -27,38 +27,55 @@ check_file_exists "${script_dir}/voice-gen-glados.py"
 # Delete logs from previous pass
 find . -name "*.log" -delete
 
-# Generate voices
-"./voice-gen.py" "./voices/cs-CZ.csv" cs-CZ-VlastaNeural cz || exit 1
-"./voice-gen.py" "./voices/da_DK.csv" da-DK-ChristelNeural da || exit 1
-"./voice-gen.py" "./voices/da_DK_scripts.csv" da-DK-ChristelNeural da/SCRIPTS || exit 1
-"./voice-gen.py" "./voices/de-DE.csv" de-DE-KatjaNeural de || exit 1
-"./voice-gen.py" "./voices/en-GB.csv" en-IE-EmilyNeural en || exit 1
-"./voice-gen.py" "./voices/en-GB_scripts.csv" en-IE-EmilyNeural en/SCRIPTS || exit 1
-"./voice-gen.py" "./voices/en-GB.csv" en-GB-LibbyNeural en_gb-libby || exit 1
-"./voice-gen.py" "./voices/en-GB_scripts.csv" en-GB-LibbyNeural en_gb-libby/SCRIPTS || exit 1
-"./voice-gen.py" "./voices/en-GB.csv" en-GB-RyanNeural en_gb-ryan || exit 1
-"./voice-gen.py" "./voices/en-GB_scripts.csv" en-GB-RyanNeural en_gb-ryan/SCRIPTS || exit 1
-"./voice-gen-glados.py" "./voices/en-GB.csv" en_gb-glados || exit 1
-"./voice-gen-glados.py" "./voices/en-GB_scripts.csv" en_gb-glados/SCRIPTS || exit 1
-"./voice-gen.py" "./voices/en-US.csv" en-US-GuyNeural en_us-guy || exit 1
-"./voice-gen.py" "./voices/en-US_scripts.csv" en-US-GuyNeural en_us-guy/SCRIPTS || exit 1
-"./voice-gen.py" "./voices/en-US.csv" en-US-MichelleNeural en_us-michelle || exit 1
-"./voice-gen.py" "./voices/en-US_scripts.csv" en-US-MichelleNeural en_us-michelle/SCRIPTS || exit 1
-"./voice-gen.py" "./voices/en-US.csv" en-US-SaraNeural en_us-sara || exit 1
-"./voice-gen.py" "./voices/en-US_scripts.csv" en-US-SaraNeural en_us-sara/SCRIPTS || exit 1
-"./voice-gen.py" "./voices/es-CL.csv" es-CL-CatalinaNeural es_cl-catalina || exit 1
-"./voice-gen.py" "./voices/es-ES.csv" es-ES-ElviraNeural es || exit 1
-"./voice-gen.py" "./voices/fr-FR.csv" fr-FR-DeniseNeural fr || exit 1
-"./voice-gen.py" "./voices/it-IT.csv" it-IT-ElsaNeural it || exit 1
-"./voice-gen.py" "./voices/it-IT_scripts.csv" it-IT-ElsaNeural it/SCRIPTS || exit 1
-"./voice-gen.py" "./voices/ja-JP.csv" ja-JP-NanamiNeural jp || exit 1
-"./voice-gen.py" "./voices/ja-JP_scripts.csv" ja-JP-NanamiNeural jp/SCRIPTS || exit 1
-"./voice-gen.py" "./voices/pt-PT.csv" pt-BR-FranciscaNeural pt || exit 1
-"./voice-gen.py" "./voices/ru-RU.csv" ru-RU-SvetlanaNeural ru || exit 1
-"./voice-gen.py" "./voices/sv-SE.csv" sv-SE-SofieNeural se --pitch "dn10%" --rate "dn0.9" || exit 1
-"./voice-gen.py" "./voices/sv-SE_scripts.csv" sv-SE-SofieNeural se/SCRIPTS --pitch "dn10%" --rate "dn0.9" || exit 1
-"./voice-gen.py" "./voices/uk-UA.csv" uk-UA-OstapNeural ua-ostap || exit 1
-"./voice-gen.py" "./voices/uk-UA.csv" uk-UA-PolinaNeural ua-polina || exit 1
-"./voice-gen.py" "./voices/zh-CN.csv" zh-CN-XiaoxiaoNeural cn || exit 1
-"./voice-gen.py" "./voices/zh-TW.csv" zh-TW-HsiaoChenNeural tw || exit 1
-"./voice-gen.py" "./voices/zh-HK.csv" zh-HK-HiuGaaiNeural hk --rate "dn1.2" || exit 1
+# Azure TTS voice configurations
+voices=(
+    "./voices/cs-CZ.csv cs-CZ-VlastaNeural cz"
+    "./voices/da_DK.csv da-DK-ChristelNeural da"
+    "./voices/da_DK_scripts.csv da-DK-ChristelNeural da/SCRIPTS"
+    "./voices/de-DE.csv de-DE-KatjaNeural de"
+    "./voices/en-GB.csv en-IE-EmilyNeural en"
+    "./voices/en-GB_scripts.csv en-IE-EmilyNeural en/SCRIPTS"
+    "./voices/en-GB.csv en-GB-LibbyNeural en_gb-libby"
+    "./voices/en-GB_scripts.csv en-GB-LibbyNeural en_gb-libby/SCRIPTS"
+    "./voices/en-GB.csv en-GB-RyanNeural en_gb-ryan"
+    "./voices/en-GB_scripts.csv en-GB-RyanNeural en_gb-ryan/SCRIPTS"
+    "./voices/en-US.csv en-US-GuyNeural en_us-guy"
+    "./voices/en-US_scripts.csv en-US-GuyNeural en_us-guy/SCRIPTS"
+    "./voices/en-US.csv en-US-MichelleNeural en_us-michelle"
+    "./voices/en-US_scripts.csv en-US-MichelleNeural en_us-michelle/SCRIPTS"
+    "./voices/en-US.csv en-US-SaraNeural en_us-sara"
+    "./voices/en-US_scripts.csv en-US-SaraNeural en_us-sara/SCRIPTS"
+    "./voices/es-CL.csv es-CL-CatalinaNeural es_cl-catalina"
+    "./voices/es-ES.csv es-ES-ElviraNeural es"
+    "./voices/fr-FR.csv fr-FR-DeniseNeural fr"
+    "./voices/it-IT.csv it-IT-ElsaNeural it"
+    "./voices/it-IT_scripts.csv it-IT-ElsaNeural it/SCRIPTS"
+    "./voices/ja-JP.csv ja-JP-NanamiNeural jp"
+    "./voices/ja-JP_scripts.csv ja-JP-NanamiNeural jp/SCRIPTS"
+    "./voices/pt-PT.csv pt-BR-FranciscaNeural pt"
+    "./voices/ru-RU.csv ru-RU-SvetlanaNeural ru"
+    "./voices/sv-SE.csv sv-SE-SofieNeural se --pitch 'dn10%' --rate 'dn0.9'"
+    "./voices/sv-SE_scripts.csv sv-SE-SofieNeural se/SCRIPTS --pitch 'dn10%' --rate 'dn0.9'"
+    "./voices/uk-UA.csv uk-UA-OstapNeural ua-ostap"
+    "./voices/uk-UA.csv uk-UA-PolinaNeural ua-polina"
+    "./voices/zh-CN.csv zh-CN-XiaoxiaoNeural cn"
+    "./voices/zh-TW.csv zh-TW-HsiaoChenNeural tw"
+    "./voices/zh-HK.csv zh-HK-HiuGaaiNeural hk --rate 'dn1.2'"
+)
+
+# Loop through the Azure TTS configurations
+for voice in "${voices[@]}"; do
+    # Use eval to handle parameters with spaces (e.g., --pitch and --rate)
+    eval "./voice-gen.py ${voice}" || exit 1
+done
+
+# GLaDOS-specific configurations
+glados_voices=(
+    "./voices/en-GB.csv en_gb-glados"
+    "./voices/en-GB_scripts.csv en_gb-glados/SCRIPTS"
+)
+
+# Loop through the GLaDOS configurations
+for glados_voice in "${glados_voices[@]}"; do
+    eval "./voice-gen-glados.py ${glados_voice}" || exit 1
+done
