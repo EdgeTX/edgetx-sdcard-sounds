0e496af53657ee2bd84e2bb4d41c89769c46400d
Peter Feerick <5500713+pfeerick@users.noreply.github.com>
2025-07-07 10:08:39 +1000

    chore: lock Speech CLI version to latest for .NET 6 SDK

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -11,7 +11,7 @@ cd .. && sudo rm -R openssl-1.1.1u
 
 # Install .NET 6.0 SDK and the Speech CLI tool
 sudo apt-get update && sudo apt-get install -y dotnet-sdk-6.0
-dotnet tool install --global Microsoft.CognitiveServices.Speech.CLI
+dotnet tool install --global Microsoft.CognitiveServices.Speech.CLI --version 1.42.0
 
 # Install the Azure Speech SDK for Python
 python -m pip install azure-cognitiveservices-speech ffmpeg-normalize
